3e4fcde7bb30a9d680c5fd8ed368ce41129cc885
Anders Bengtsson <anders.bengtsson@conrab.se>
2021-03-09 11:31:30 +0100

    wip

---
+++ config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCpBodxgjBpcoRXB4OXz1df5NJc6McIRQw",
+    authDomain: "skillzta-46012.firebaseapp.com",
+    databaseURL: "https://skillzta-46012-default-rtdb.firebaseio.com",
+    projectId: "skillzta-46012",
+    storageBucket: "skillzta-46012.appspot.com",
+    messagingSenderId: "402988858172",
+    appId: "1:402988858172:web:cd8575754bd838644cfc39",
+    measurementId: "G-JEQZ7SKE1X"
+  };
